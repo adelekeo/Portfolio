34efa108e835a4eaf2ba26763420d40d5f7cccc9
Oluwole Adeleke <oluwoleadeleke76@gmail.com>
2025-06-13 18:13:43 +0000

    Second Commit

---
+++ ch03/HandlingExceptions/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+WriteLine("Before parsing");
+Write("What is your age? ");
+string? input = ReadLine();
+
+ // For production  purposes
+if (input is  null)
+{
+    WriteLine("Wrong entry, app ends");
+    return;
+}
+try
+{
+  int age = int.Parse(input!);
+  WriteLine($"You are {age} years old.");
+}
+catch(FormatException x)
+{
+    WriteLine("The entered  age is not a valid format");
+}
+catch(Exception ex)
+{
+     WriteLine($"{ex.GetType()} says {ex.Message}");
+}
+WriteLine("After parsing");
