9a8e05371bdf1f4f151d5a877b908c0be52fae9b
Oluwole Adeleke <oluwoleadeleke76@gmail.com>
2025-09-03 11:12:31 -0400

    Code committed on 9/3

---
+++ Chapter08/WorkingWithRanges/Program.cs
@@ -0,0 +1,20 @@
+﻿string name = "Delee Adeleke";
+
+// Get the legths of the first and last names.
+
+int LengthOfFirst = name.IndexOf(' ');
+int LengthOfLast = name.Length - LengthOfFirst - 1;
+WriteLine($"First name length: {LengthOfFirst}, Last name length: {LengthOfLast}");
+WriteLine($"Total length: {name.Length}");
+
+// Using Substring.
+string firstName = name.Substring(
+  startIndex: 0,
+  length: LengthOfFirst);
+string lastName = name.Substring(startIndex: name.Length - LengthOfLast,  length: LengthOfLast);
+WriteLine($"First: {firstName}, Last: {lastName}");
+// Using spans.
+ReadOnlySpan<char> nameAsSpan = name.AsSpan();
+ReadOnlySpan<char> firstNameSpan = nameAsSpan[0..LengthOfFirst];
+ReadOnlySpan<char> lastNameSpan = nameAsSpan[^LengthOfLast..];
+WriteLine($"First: {firstNameSpan}, Last: {lastNameSpan}");
---
+++ Chapter08/WorkingWithRegularExpressions/Program.Strings.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis; // To use [StringSyntax].
+partial class Program
+{
+    [StringSyntax(StringSyntaxAttribute.Regex)]
+    private const string DigitsOnlyText = @"^\d+$";
+    [StringSyntax(StringSyntaxAttribute.Regex)]
+    private const string CommaSeparatorText =
+      "(?:^|,)(?=[^\"]|(\")?)\"?((?(1)[^\"]*|[^,\"]*))\"?(?=,|$)";
+}
